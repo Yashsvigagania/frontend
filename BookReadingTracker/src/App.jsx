ede346ddfc255a20bc525b06b1f5c71ecf8d9100
Yashsvigagania <gaganiayashsvi@gmail.com>
2026-03-18 22:59:05 +0530

    commit it

--- BookReadingTracker/src/App.jsx
+++ BookReadingTracker/src/App.jsx
@@ -1,54 +1,56 @@
 import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from './assets/vite.svg'
-//import heroImg from './assets/hero.png'
 import './App.css'
-import {AddBook} from "./AddBook";
-import {DeleteBook} from "./DeleteBook";
-import {ToggleStatus} from "./ToggleStatus";
-import { DeleteItem } from '../../GroceryListManager/src/Components/DeleteItem';
+
+import AddBook from "./AddBook";
+import DeleteBook from "./DeleteBook";
+import ToggleStatus from "./ToggleStatus";
 
 function App() {
-  const[items,setItems]=useState([]);
-  const[input,setInput]=useState("");
-  const[error,setError]=useState("");
+  const [items, setItems] = useState([]);
+  const [input, setInput] = useState("");
+  const [error, setError] = useState("");
 
   return (
     <div>
-      <h2>Bood Reading Manager</h2>
+      <h2>Book Reading Manager</h2>
 
       <input
-      type="text"
-      value={input}
-      onChange={(e)=> setInput(e.target.value)}>
-      </input>
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
 
-      <button onClick={()=>
-        AddBook(items,input,setItems,setInput,setError)
-      }>
-      Add Book
+      <button
+        onClick={() =>
+          AddBook(items, input, setItems, setInput, setError)
+        }
+      >
+        Add Book
       </button>
+
       <p style={{ color: "red" }}>{error}</p>
 
       <ul>
-        {items.map((item,index)=>(
+        {items.map((item, index) => (
           <li key={index}>
-            {item.name}-{item.read ? "Reading": "Completed"}
+            {item.name} - {item.read ? "Reading" : "Completed"}
 
-            <button 
-            onClick={()=> ToggleStatus(index,item,setItems)}>
-              {item.read ? "Mark Reading":"Mark Completed"}
+            <button
+              onClick={() => ToggleStatus(index, items, setItems)}
+            >
+              {item.read ? "Mark Completed" : "Mark Reading"}
             </button>
 
             <button
-            onClick={()=> DeleteBook(index,item,setItems)}>
+              onClick={() => DeleteBook(index, items, setItems)}
+            >
               Delete Book
             </button>
           </li>
         ))}
       </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
