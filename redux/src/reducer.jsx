e4f39fb169012b64682e0bf596893680dd82fdc4
Yashsvigagania <gaganiayashsvi@gmail.com>
2026-04-29 15:07:50 +0530

    redux

--- redux/src/reducer.jsx
+++ redux/src/reducer.jsx
@@ -1,7 +1,42 @@
-const initializer={
-    todo : []
-    theme:"light",
-    user:(name:"Guest"),
-    lang:"en"
-}
+import { ADD_TODO, DELETE_TODO, TOGGLE } from "./action";
 
+const initialState = {
+  todo: []
+};
+
+export const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    
+    case ADD_TODO:
+      return {
+        ...state,
+        todo: [
+          ...state.todo,
+          {
+            id: Date.now(),
+            text: action.payload,
+            completed: false
+          }
+        ]
+      };
+
+    case DELETE_TODO:
+      return {
+        ...state,
+        todo: state.todo.filter(item => item.id !== action.payload)
+      };
+
+    case TOGGLE:
+      return {
+        ...state,
+        todo: state.todo.map(item =>
+          item.id === action.payload
+            ? { ...item, completed: !item.completed }
+            : item
+        )
+      };
+
+    default:
+      return state;
+  }
+};
